b16abae3d157b476dc592ea2a1330c9c1cc7c5cc
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-02-26 23:47:10 +0000

    finished with the design of the frontend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React from "react";
-
-import Schools from "./components/schools/Schools";
 import Home from "./pages/home/Home";
+import Ucc from "./pages/ucc/Ucc";
+import Knust from "./pages/knust/Knust";
+import Winneba from "./pages/winneba/Winneba";
+import Leg from "./pages/leg/Leg";
+
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/ucc" element={<Ucc />} />
+        <Route path="/leg" element={<Leg />} />
+        <Route path="/knust" element={<Knust />} />
+        <Route path="/uew" element={<Winneba />} />
+      </Routes>
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,43 +4,53 @@ import WINNEBA from "../../images/winneba.png";
 import LEGON from "../../images/ug.jpg";
 import KNUST from "../../images/knust.jpg";
 import Footer from "../../components/footer/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <>
-      <div className="home">
-        <div className="top">
-          <span className="logo">SchoolChecker</span>
-        </div>
-        <div className="center">
-          <p className="desc">
-            Select a school below to know the kind of programmes you qualify for
-            ...
-          </p>
-          <div className="schools">
+    <div className="home">
+      <div className="top">
+        <span className="logo">SchoolChecker</span>
+      </div>
+      <div className="center">
+        <p className="desc">
+          Select a school below to know the kind of programmes you qualify for
+          ...
+        </p>
+        <div className="schools">
+          <Link to="/ucc" style={{ textDecoration: "none", color: "inherit" }}>
             <div className="school">
               <img src={UCC} alt="" />
               <h1 className="name">university of Cape Coast</h1>
             </div>
+          </Link>
+          <Link to="/uew" style={{ textDecoration: "none", color: "inherit" }}>
             <div className="school">
               <img src={WINNEBA} alt="" />
               <h1 className="name">university of Winneba</h1>
             </div>
+          </Link>
+          <Link to="/leg" style={{ textDecoration: "none", color: "inherit" }}>
             <div className="school">
               <img src={LEGON} alt="" />
               <h1 className="name">university of Ghana</h1>
             </div>
+          </Link>
+          <Link
+            to="/knust"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
             <div className="school">
               <img src={KNUST} alt="" />
               <h1 className="name">
                 kwame nkrumah university of science and technology
               </h1>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
